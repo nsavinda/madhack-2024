b3de6ccb12dcc0b51b79931006d9126794386d24
lakshith <lakshith.k.nishshanke@gmail.com>
2023-12-27 00:31:51 +0530

    Hero section WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import './App.css'
-import NavBar from "./Components/NavBar/NavBar"
+import React from 'react';
+import './App.scss';
+import logo from './assets/logo.png'
+import hero from './assets/hero.png';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-container">
+            <header className="header">
+                <nav className="navbar">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <ul>
+                        <li><a href="#about">about</a></li>
+                        <li><a href="#guidelines">guidelines</a></li>
+                        <li><a href="#timeline">timeline</a></li>
+                        <li><a href="#prizes">prizes</a></li>
+                        <li><a href="#merchandise">merchandise</a></li>
+                        <li><a href="#contact">contact us</a></li>
+                        <li><a href="#register">register</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="main-content">
+                <div className="text-buttons">
+                    <h1>Battle out your Mobile App Development Skills</h1>
+                    <div className="buttons">
+                        <button className="register-btn">Register Now</button>
+                        <button className="learn-more-btn">Learn More</button>
+                    </div>
+                </div>
+                <div className="hero-image">
+                    <img src={hero} alt="Hero" />
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
