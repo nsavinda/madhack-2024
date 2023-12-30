f8c6ebf69a54062d804c368a0625a81fed4cdb85
nsavinda <nirmalsavinda29@gmail.com>
2023-12-30 09:01:41 +0530

    feat:timeline

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import "@fontsource/ibm-plex-mono";
 import NavBar from "./Components/NavBar/NavBar"
+import Timeline from "./Components/Timeline/Timeline"
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Timeline />
     </div>
   );
 }
---
+++ frontend/src/Components/data.ts
@@ -0,0 +1,28 @@
+const timelineData = [
+  {
+    title: "Lorem Ipsum Event 1",
+    iconBg: "#383E56",
+    date: "January 2022 - March 2023",
+    point: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    title: "Lorem Ipsum Event 2",
+    iconBg: "#E6DEDD",
+    date: "2018 -  2022",
+    point: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    title: "Lorem Ipsum Event 3",
+    iconBg: "#383E56",
+    date: "2016 -  2018",
+    point: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  },
+  {
+    title: "Lorem Ipsum Event 4",
+    iconBg: "#E6DEDD",
+    date: "2004 - 2016",
+    point: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+];
+
+export { timelineData };
