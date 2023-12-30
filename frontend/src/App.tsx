ac13d7c35b121d9f3980acdcc01c0ea2031bfd15
nsavinda <nirmalsavinda29@gmail.com>
2023-12-30 11:48:18 +0530

    feat:prizes section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 // import React from 'react'
 // import './App.css'
-// import "@fontsource/ibm-plex-mono";
 // import NavBar from "./Components/NavBar/NavBar"
 
 
@@ -13,19 +12,25 @@
 //   );
 import React from 'react';
 import './App.scss';
+import "@fontsource/ibm-plex-mono";
+
+
 import logo from './assets/logo.png'
 import hero from './assets/hero.png';
 import NavBar from './Components/NavBar/NavBar';
 import Timeline from "./Components/Timeline/Timeline"
 import Hero from './Components/Hero/Hero';
+import Prizes from './Components/Prizes/Prizes';
 
 
 const App = () => {
     return (
         <div className="app-container">
             <NavBar />
-        <Hero />
+            <Hero />
             <Timeline />
+
+            <Prizes />
         </div>
     );
 }
