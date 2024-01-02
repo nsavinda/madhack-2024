3b91a225ec679f3ede08ef2d9f1030f50cba61c7
lakshith <lakshith.k.nishshanke@gmail.com>
2024-01-02 18:44:09 +0530

    change prize section styles

--- frontend/src/Components/Prizes/Prizes.tsx
+++ frontend/src/Components/Prizes/Prizes.tsx
@@ -6,38 +6,52 @@ import img3 from '../../assets/prizes/3.png';
 import React, { ReactNode } from 'react';
 
 import './Prizes.scss';
+import Faq from "react-faq-component";
 
 
 
 function Prizes() {
   return (
-    <div className="prizes">
-        <h1>Prizes</h1>
+      <div className="site-section local-bootstrap remove-top-pad">
+          <div className="container">
+              <div className="row">
+                  <div className="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+                      <div className="site-section-heading">
+                          <h2>
+                              <span>Prizes</span>
+                          </h2>
+                      </div>
+                  </div>
+              </div>
+              <div className="row align-items-center speaker">
+                  <div className="prizes">
+                      <div className="prizes-container">
+                          <div className="prize">
+                              <img className='prize-image' src={img1} alt="Prize 1" />
+                              <h2>Silver</h2>
+                              <p>Prize 1 description</p>
 
-        <div className="prizes-container">
-            <div className="prize">
-                <img className='prize-image' src={img1} alt="Prize 1" />
-                <h2>Prize 1</h2>
-                <p>Prize 1 description</p>
+                          </div>
+                          <div className="prize">
+                              <img className='prize-image' src={img2} alt="Prize 1" />
+                              <h2>Gold</h2>
+                              <p>Prize 1 description</p>
 
-                </div>
-                <div className="prize">
-                <img className='prize-image' src={img1} alt="Prize 1" />
-                <h2>Prize 1</h2>
-                <p>Prize 1 description</p>
+                          </div>
+                          <div className="prize">
+                              <img className='prize-image' src={img3} alt="Prize 1" />
+                              <h2>Bronze</h2>
+                              <p>Prize 1 description</p>
 
-                </div>
-                <div className="prize">
-                <img className='prize-image' src={img1} alt="Prize 1" />
-                <h2>Prize 1</h2>
-                <p>Prize 1 description</p>
+                          </div>
+                      </div>
 
-                </div>
-            </div>
-     
 
-    
-    </div>
+
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
