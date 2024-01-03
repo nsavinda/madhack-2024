9d0e5fb3d34177ef0549b3fe2f065fb8f3a2658d
nsavinda <nirmalsavinda29@gmail.com>
2024-01-03 15:44:21 +0530

    whatsapp

--- frontend/src/Components/Registration/Registration.tsx
+++ frontend/src/Components/Registration/Registration.tsx
@@ -21,7 +21,7 @@ export default function Registration() {
       background: '#232631',
       color: '#fff',
       // add link to whatsapp group with whatsapp icon
-      footer: '<a href="https://chat.whatsapp.com/J2NvQ7k8bJwJ8K4tJ9b1eY" target="_blank"> Join our whatsapp group <br> <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png"/></a>'
+      footer: '<a href="https://chat.whatsapp.com/LJ7CwK4eiRd3jkLntBfoj0" target="_blank"> Join our whatsapp group <br> <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png"/></a>'
     })
 
     
